60195a1e5474f5d0b95ea85dbbe0c8da7b552691
wmji <maoji.wang@cims.nyu.edu>
2017-05-17 00:49:51 -0400

    pack board in a class

--- board.py
+++ board.py
@@ -16,81 +16,100 @@
 #  ==============================================
 
 
-UI_board_size = 19  # the board is 19x19
+board_size = 19  # the board is 19x19
 
 
-def UI_array2string(x): return ' '.join(x)  # to process string<->array
+class Board:
+    # to display the board in terminal
+
+    current_state = zeros([board_size,board_size])
 
 
-def UI_draw_board(x, o):
-    """
-      to draw the board on the screen, and put pieces in.
-      x is the piece positions of the first player (black, moves first)
-      o is the second player
-      both x and o are len()*2 arrays, meaning [line, col], 1 to 19.
-    """
+    def __init__(self, bdst = zeros([board_size,board_size])):
+        # bdst is the inputing board-state
+        self.current_state = bdst.copy()
 
-    UI_edge_char = ' '  # around the board
-    UI_grid_char = '.'  # empty on board
-    UI_star_char = '+'  # 9 star positions
-    UI_1st_char = 'x'
-    UI_2nd_char = 'o'
 
-    # an empty board with left/right edges
-    UI_board = array([ \
-             list(str(i).zfill(2)+UI_grid_char*UI_board_size+str(i).zfill(2)) \
-             for i in range(UI_board_size+2) ])
+    def setto(self, bdst):
+        self.current_state = bdst.copy()
 
-    # polish the stars
-    UI_board[ 4, 5] = UI_star_char
-    UI_board[ 4,-6] = UI_star_char
-    UI_board[-5, 5] = UI_star_char
-    UI_board[-5,-6] = UI_star_char
-    UI_board[10,11] = UI_star_char
-    UI_board[ 4,11] = UI_star_char
-    UI_board[10,-6] = UI_star_char
-    UI_board[-5,11] = UI_star_char
-    UI_board[10, 5] = UI_star_char
 
-    # polish the upper/bottom edges
-    #UI_board[0] = UI_edge_char
-    #UI_board[-1] = UI_edge_char
-    for i in range(UI_board_size):
-        UI_board[0, i+2] = string.ascii_uppercase[i]
-        UI_board[-1,i+2] = string.ascii_uppercase[i]
+    def reset(self):
+        self.current_state = zeros([board_size,board_size])
 
-    # polish the corners
-    UI_board[0, 0] = UI_edge_char
-    UI_board[0, 1] = UI_edge_char
-    UI_board[-1,0] = UI_edge_char
-    UI_board[-1,1] = UI_edge_char
-    UI_board[0, -1] = UI_edge_char
-    UI_board[0, -2] = UI_edge_char
-    UI_board[-1,-1] = UI_edge_char
-    UI_board[-1,-2] = UI_edge_char
 
-    for i0,i1 in x:
-        if i0<=0 or i>UI_board_size:  # check before plotting
-            print("error in board plotting 1st player", i0,i1)
-            exit(-1)
-        UI_board[i0,i1+1] = UI_1st_char  # y add 1 for edges
+    def display(self):
+        """
+        to draw the board on the screen, and put pieces in.
+        x is the piece positions of the first player (black, moves first)
+        o is the second player
+        both x and o are len()*2 arrays, meaning [line, col], 1 to 19.
+        """
 
-    for i0,i1 in o:
-        if i0<=0 or i>UI_board_size:
-            print("error in board plotting 2nd player", i0,i1)
-            exit(-1)
-        UI_board[i0,i1+1] = UI_2nd_char  # y add 1 for edges
+        def array2string(x):
+            return ' '.join(x)  # to process string<->array
 
-    os.system('clear')
+        edge_char = ' '  # around the board
+        grid_char = '.'  # empty on board
+        star_char = '+'  # 9 star positions
+        fst_char = 'x'
+        scd_char = 'o'
 
-    print()
-    print()
-    for i in UI_board:
-        print(UI_array2string(i).center(1))
-    print()
+        # an empty board with left/right edges
+        UI_board = array([ \
+                list(str(i).zfill(2)+grid_char*board_size+str(i).zfill(2)) \
+                for i in range(board_size+2) ])
+
+        # polish the stars
+        UI_board[ 4, 5] = star_char
+        UI_board[ 4,-6] = star_char
+        UI_board[-5, 5] = star_char
+        UI_board[-5,-6] = star_char
+        UI_board[10,11] = star_char
+        UI_board[ 4,11] = star_char
+        UI_board[10,-6] = star_char
+        UI_board[-5,11] = star_char
+        UI_board[10, 5] = star_char
+
+        # polish the upper/bottom edges
+        #UI_board[0] = UI_edge_char
+        #UI_board[-1] = UI_edge_char
+        for i in range(board_size):
+            UI_board[0, i+2] = string.ascii_uppercase[i]
+            UI_board[-1,i+2] = string.ascii_uppercase[i]
+
+        # polish the corners
+        UI_board[0, 0] = edge_char
+        UI_board[0, 1] = edge_char
+        UI_board[-1,0] = edge_char
+        UI_board[-1,1] = edge_char
+        UI_board[0, -1] = edge_char
+        UI_board[0, -2] = edge_char
+        UI_board[-1,-1] = edge_char
+        UI_board[-1,-2] = edge_char
+
+        for i0,i1 in muloop([board_size, board_size]):
+            if self.current_state[i0,i1] == 1:
+                UI_board[i0+1,i1+2] = fst_char  # y add 1 for edges
+            elif self.current_state[i0,i1] == 2:
+                UI_board[i0+1,i1+2] = scd_char  # y add 1 for edges
+
+        os.system('clear')
+        print()
+        print()
+        for i in UI_board:
+            print(array2string(i).center(1))
+        print()
+
 
 
 if __name__ == "__main__":
-    x=array([[6,4], [3,3], [1,8], [4,3], [1,12], [3,4], [12,1]])
-    o=array([[2,1], [1,7], [3,5], [3,6], [1,3], [18,4], [15,3]])
-    UI_draw_board(x,o)
+    x=array([[6,4], [3,3], [1,8], [4,3], [1,19], [3,4], [12,1]])
+    o=array([[2,1], [1,7], [3,5], [3,6], [1,3], [19,4], [16,4]])
+
+    bdst = zeros([board_size, board_size])
+    for i0,i1 in x: bdst[i0-1,i1-1] = 1  # -1 to convert to matrix
+    for i0,i1 in o: bdst[i0-1,i1-1] = 2  #    to count from 0
+
+    bd = Board(bdst)
+    bd.display()
